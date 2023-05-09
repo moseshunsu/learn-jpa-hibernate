bc88e68937aa4c4fc7cd602dd2680c3865a96ef5
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-09 14:48:57 +0100

    final commit

--- src/main/java/com/higherAchievers/springboot/learnjpaandhibernate/course/Course.java
+++ src/main/java/com/higherAchievers/springboot/learnjpaandhibernate/course/Course.java
@@ -1,7 +1,14 @@
 package com.higherAchievers.springboot.learnjpaandhibernate.course;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+
+    @Id
     private long id;
+
     private String name;
     private String author;
 
---
+++ src/main/java/com/higherAchievers/springboot/learnjpaandhibernate/course/springDataJpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,12 @@
+package com.higherAchievers.springboot.learnjpaandhibernate.course.springDataJpa;
+
+import com.higherAchievers.springboot.learnjpaandhibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByAuthor(String author);
+    List<Course> findByName(String name);
+}
